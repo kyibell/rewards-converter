40fbec680bdf104f6c72e4f5a2e3f3223ae925b3
kyibell <148383705+kyibell@users.noreply.github.com>
2024-08-07 13:06:51 -0500

    Completed Task One by Implementing RewardValue class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class RewardValue {
+
+    private double cashValue;
+    private int milesValue;
+    public static final double CONVERSION_RATE = 0.0035;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = (int)(cashValue / CONVERSION_RATE);
+    }
+
+    public RewardValue(int milesValue) {
+        this.milesValue = milesValue;
+       this.cashValue = milesValue * CONVERSION_RATE;
+    }
+
+    public double getCashValue() {
+        return this.cashValue;
+    }
+
+    public int getMilesValue() {
+       return this.milesValue;
+    }
 }
